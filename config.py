e5fe8788767712153784b9a473f895ba91ddd335
toxel <timo625@gmail.com>
2013-08-07 01:02:31 +0200

    Edited default config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 DATABASE = 'scores.db'
 DEBUG = True
 
-SECRET_KEY = 'itsaSECRET' # YOU SHOULD CHANGE THIS
+SECRET_KEY = 'secret' # YOU SHOULD CHANGE THIS
 
-USERNAME_1 = 'jele'
+USERNAME_1 = 'gamer1'
 PASSWORD_1 = 'passwort'
-USERNAME_2 = 'timo'
+USERNAME_2 = 'gamer2'
 PASSWORD_2 = 'passwort'
 
 TRAP_BAD_REQUEST_ERRORS = True
