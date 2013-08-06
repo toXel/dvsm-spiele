e597786c4fe1599f595068d372902c0931ce5830
toxel <timo625@gmail.com>
2013-08-07 01:06:40 +0200

    Edited SQL schema for variable gamer names

--- schema.sql
+++ schema.sql
@@ -3,6 +3,8 @@ create table scores (
 	gameid integer primary key autoincrement,
 	gametitle string not null,
 	gameurl string not null,
+	gamer1name string not null,
+	gamer2name string not null,
 	gamer1score string not null,
 	gamer2score string not null,
 	higherisbetter integer not null default 1,
