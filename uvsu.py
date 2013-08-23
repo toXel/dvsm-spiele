434abf81cb668c4152dd80e8151bda2e6f621b32
toXel <timo625@gmail.com>
2013-08-24 00:43:34 +0200

    Removed unused import

--- uvsu.py
+++ uvsu.py
@@ -3,7 +3,6 @@
 import sqlite3
 from contextlib import closing
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
-import os
 
 app = Flask(__name__)
 app.config.from_object(__name__)
